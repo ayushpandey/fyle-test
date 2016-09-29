01c4140648c7c7568d59d454e7f3b81f3b25875d
Ayush Pandey <ayush@datadoesgood.com>
2016-09-29 22:36:59 +0530

    initialize

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('fyle', []);
+
+app.controller('searchCtrl', ['$scope','$http',function($scope,$http) {
+  $scope.data={
+    cities: [
+      {id: 'BANGALORE', name: 'Bangalore'},
+      {id: 'KOLKATA', name: 'Kolkata'},
+      {id: 'DELHI', name: 'Delhi'},
+      {id: 'MUMBAI', name: 'Mumbai'},
+      {id: 'CHANDIGARH', name: 'Chandigarh'}
+    ]};
+    $scope.itemChanged = function() {
+      var url="https://app.fyle.in/api/bank_branches?city="+$scope.selectedCity+"&offset=0";
+      console.log(url);
+      $http.get(url).success(function(response){
+        console.log(response);
+        $scope.banks=response;
+      });
+
+      console.log($scope.selectedCity);
+    }
+}]);
